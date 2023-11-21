9591b049a0c3759ad65a61f02935e560532c6d1c
Joseph Musenge <81922031+JosephMusenge@users.noreply.github.com>
2023-11-21 01:26:20 -0500

    completed backend file

--- backend/main.py
+++ backend/main.py
@@ -43,41 +43,41 @@ async def get_groceries_list():
     return grocery_list
 
 
-# # Make a food class
-# class Food(BaseModel):
-#     id:str
-#     name:str
+# Make a food class
+class Food(BaseModel):
+    id:str
+    name:str
 
-# # Add new item to grocery list
-# @app.post('/add') # Using a post request cause we are updating and not getting
-# async def add_item(item:Food): # The item parameter should be of type class, same type better results
-#     grocery_list.append(item)
-#     return "Added " + item.name
-
-
-# @app.get('/recipes')
-# async def find_recipes(item:Food):
-#     result = []
-#     response = find_by_ingredients(item.name)
-#     for recipes in response:
-#         recipe = {}
-#         recipe["title"] = recipes["title"]
-#         recipe["image"] = recipes["image"]
-#         result.append(recipe)
-#     return result
+# Add new item to grocery list
+@app.post('/add') # Using a post request cause we are updating and not getting
+async def add_item(item:Food): # The item parameter should be of type class, same type better results
+    grocery_list.append(item)
+    return "Added " + item.name
 
 
-# def find_by_ingredients(ingredients):
-#     url = 'https://api.spoonacular.com/recipes/findByIngredients'
-#     headers = {
-#         'Content-Type': 'application/json',
-#         'x-api-key':os.getenv('SPOONACULAR_API_KEY') }
-#     parameters ={
-#         'ingredients': ingredients}
-#     response = requests.get(url, headers=headers, params=parameters)
-#     if (response.status_code == 200):
-#         print("The request was a success!")
-#     elif (response.status_code == 404):
-#         print("Result not found!")
-#     return response.json()
+@app.get('/recipes')
+async def find_recipes(item:Food):
+    result = []
+    response = find_by_ingredients(item.name)
+    for recipes in response:
+        recipe = {}
+        recipe["title"] = recipes["title"]
+        recipe["image"] = recipes["image"]
+        result.append(recipe)
+    return result
+
+
+def find_by_ingredients(ingredients):
+    url = 'https://api.spoonacular.com/recipes/findByIngredients'
+    headers = {
+        'Content-Type': 'application/json',
+        'x-api-key':os.getenv('SPOONACULAR_API_KEY') }
+    parameters ={
+        'ingredients': ingredients}
+    response = requests.get(url, headers=headers, params=parameters)
+    if (response.status_code == 200):
+        print("The request was a success!")
+    elif (response.status_code == 404):
+        print("Result not found!")
+    return response.json()
     
