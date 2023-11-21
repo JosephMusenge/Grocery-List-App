d9375eb35257135c9c39e9e3676ea629aa856249
Joseph Musenge <81922031+JosephMusenge@users.noreply.github.com>
2023-11-21 01:48:33 -0500

    created List and ListItem components

--- frontend/src/components/List.tsx
+++ frontend/src/components/List.tsx
@@ -2,19 +2,23 @@ import ListItem from './ListItem';
 import React from 'react';
 import { FoodList, FoodObj } from '../../types';
 
+// Define the properties that the List component expects to receive
 interface Props {
-    list: FoodList | null,
-    updateFunc: any | null
+    list: FoodList | null;   // The list of food items to display
+    updateFunc: any | null;  // A function to be passed down to ListItem for handling item clicks
 }
 
-export default function List({list, updateFunc}: Props) {
+// Functional component representing a list of items
+export default function List({ list, updateFunc }: Props) {
     return (
+        // Render a column containing an unordered list
         <div className="col">
             <ul>
                 {
+                    // Map through each item in the list and render a ListItem component for each
                     list?.list?.map((food: FoodObj, idx: any) => <ListItem item={food} updateFunc={updateFunc} />)
                 }
             </ul>
         </div>
-    )
+    );
 }
--- frontend/src/components/ListItem.tsx
+++ frontend/src/components/ListItem.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { FoodObj } from '../../types';
 
+// Define the properties that the ListItem component expects to receive
 interface Props {
-    item: FoodObj;
-    updateFunc(ingredient: FoodObj): any | null
+    item: FoodObj;                    // The individual food item to display
+    updateFunc(ingredient: FoodObj): any | null;  // A function to be called when the item is clicked
 }
 
+// Functional component representing a single item in the list
 export default function ListItem({ item, updateFunc }: Props) {
     return (
+        // Render a clickable card for the food item
         <li className="card" onClick={() => { updateFunc(item) }}>
-            { item.name }
+            { item.name }  {/* Display the name of the food item */}
         </li>
     );
 }
