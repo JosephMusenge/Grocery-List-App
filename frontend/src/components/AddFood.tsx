10e903d995f92e84918b66037a54344197d946f7
Joseph Musenge <81922031+JosephMusenge@users.noreply.github.com>
2023-11-21 14:21:50 -0500

    created addFood and recipes components

--- frontend/src/components/AddFood.tsx
+++ frontend/src/components/AddFood.tsx
@@ -2,26 +2,35 @@ import React, { useState } from "react";
 import { MouseEventHandler } from "react";
 import '../index.css';
 
+// Define the properties that the AddModal component expects to receive
 type Props = {
-    onClose: MouseEventHandler,
-    onAdd: (foodName: string) => void
+    onClose: MouseEventHandler;             // Function to close the modal
+    onAdd: (foodName: string) => void;      // Function to add a new food item
 }
 
+// Functional component representing an Add Item modal
 function AddModal(props: Props) {
+    // State variable to track the entered food name
     const [foodName, setFoodName] = useState('');
 
+    // Function to update the food name as it is typed in the input field
     function changeFoodName(event: React.ChangeEvent<HTMLInputElement>) {
         setFoodName(event.target.value);
     }
 
-    // Two ways to define functions
+    // Render the Add Item modal
     return (
         <div>
-            <input aria-label="foodName" onChange={changeFoodName}type="text" />
+            {/* Input field for entering the food name */}
+            <input aria-label="foodName" onChange={changeFoodName} type="text" />
+
+            {/* Close button to close the modal */}
             <button className='btn' onClick={props.onClose}>Close</button>
+
+            {/* Add button to add the entered food name */}
             <button className='btn' onClick={() => {props.onAdd(foodName);} }>Add</button>
         </div>
-    )
+    );
 }
 
 export default AddModal;
--- frontend/src/components/Recipes.tsx
+++ frontend/src/components/Recipes.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { RecipeObj } from '../../types';
 
+// Define the properties that the Recipe component expects to receive
 interface Props {
-    recipe: RecipeObj | null,
+    recipe: RecipeObj | null;   // The recipe object to display, or null if no recipe is selected
 }
 
-export default function Recipe({recipe}: Props) {
+// Functional component representing a recipe display
+export default function Recipe({ recipe }: Props) {
     return (
+        // Render a column containing the recipe name and image
         <div className="col">
-            {recipe?.name}
-            <img src={recipe?.image} alt={recipe?.name} />
+            {recipe?.name}  {/* Display the name of the recipe, if it exists */}
+            <img src={recipe?.image} alt={recipe?.name} />  {/* Display the image of the recipe, if it exists */}
         </div>
     );
 }
